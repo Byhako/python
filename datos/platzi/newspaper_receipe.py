419ee39e23b7ca278d56e356fbf075b2691ff8ba
Ruben E. Acosta <eldragondera1@gmail.com>
2018-12-28 15:40:40 -0500

    limpiando datos

--- datos/platzi/newspaper_receipe.py
+++ datos/platzi/newspaper_receipe.py
@@ -3,6 +3,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 from urllib.parse import urlparse
 import pandas as pd
+import hashlib
 
 logger = logging.getLogger(__name__)
 
@@ -12,6 +13,9 @@ def main(filename):
     newspaper_uid = _extract_newspaper_uid(filename)
     df = _add_newspaper_uid_column(df, newspaper_uid)
     df = _extract_host(df)
+    df = _fill_missing_titles(df)
+    df = _generate_uids_for_rows(df)
+    df = _remove_newlines_from_body(df)
     return df
 
 
@@ -38,6 +42,43 @@ def _extract_host(df):
     df['host'] = df['url'].apply(lambda url: urlparse(url).netloc)
     return df
 
+
+def _fill_missing_titles(df):
+    logger.info('Filling missing titles')
+    missing_titles_mask = df['title'].isna()
+    missing_titles = (df[missing_titles_mask]['url']
+                        .str.extract(r'(?P<missing_titles>[^/]+)$')
+                        .apply(lambda title: title.split('-'))
+                        .apply(lambda title_list: ' '.join(title_list))
+        )
+    
+    df.loc[missing_titles_mask, 'title'] = missing_titles
+    return df
+
+
+def _generate_uids_for_rows(df):
+    logger.info('Generating uids for each rows.')
+    uids = (df
+            .apply(lambda row:hashlib.md5(bytes(row['url'].encode())), axis=1)
+            .apply(lambda hash_object: hash_object.hexdigest())
+            )
+
+    df['uid'] = uids
+    
+    return df
+
+
+def _remove_newlines_from_body(df):
+    logger.info('Remove newlines from body.')
+    stripped_body = (df
+                     .apply(lambda row: row['body'], axis=1)
+                     .apply(lambda body: list(body))
+                     .apply(lambda letters: list(map(lambda letter: letter.replace('\n', ''), letters)))
+                     .apply(lambda letters: ''.join(letters))
+                    )
+
+    df['body'] = stripped_body
+    return df
 
 
 if __name__ == "__main__":
